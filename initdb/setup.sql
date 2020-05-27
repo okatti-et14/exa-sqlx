3523c7b713e718757f6c3328df8d9fcbde5b7e9e
takumi.iioka <takumi.iioka@ebisol.co.jp>
2020-05-27 11:26:18 +0900

    初期クエリ追記

--- initdb/setup.sql
+++ initdb/setup.sql
@@ -5,6 +5,10 @@ create table users
     insert_date timestamp with time zone,
     update_date timestamp with time zone
 );
-
+create table user_names
+(
+    user_id text UNIQUE,
+    user_name text
+)
 --update users set user_id='aaa', insert_date='2020/04/11 11:11:11';--
 --insert into users values('bbb','bbb','2020/04/11 11:11:11','2020/04/11 11:11:11')
